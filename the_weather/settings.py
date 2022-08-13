5ebd3b29e90ed1e0f39571849a48cb1fe93460ed
alex23090 <alex2003199604@gmail.com>
2022-08-13 18:54:15 +0300

    Fixed bugs

--- the_weather/settings.py
+++ the_weather/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-$5spy9qfl6_6atv!3a)-z3u9fbo4h@ir+8)4ffn#ea4c^fzv@n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://the-weather-for-your-city.herokuapp.com/', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
@@ -123,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if os.getcwd() == '/app':
+    DEBUG = False
